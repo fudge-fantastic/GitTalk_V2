9cf7b2193c3e76127cc2906fef469ccce9dc8fd3
fudge-fantastic <adi.pandagle@gmail.com>
2025-05-16 10:40:55 +0530

    WIP: github.server.ts

--- app/models/github.server.ts
+++ app/models/github.server.ts
@@ -1,15 +1,21 @@
 import { Octokit } from "octokit";
+import dotenv from "dotenv";
+dotenv.config();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 if (!GITHUB_TOKEN) {
   throw new Error(
     "GitHub token not found. Please set GITHUB_TOKEN environment variable."
   );
+} else {
+  console.log("All good! Github token found.");
 }
 
 export const octokit = new Octokit({ auth: GITHUB_TOKEN });
 if (!octokit) {
   throw new Error("Error creating Octokit instance.");
+} else {
+  console.log("All good! Octokit instance created.");
 }
 
 type Response = {
@@ -20,26 +26,37 @@ type Response = {
   committedAt?: string;
 };
 
-export async function getCommitHashes(githubUrl: string): Promise<Response[]> { 
+export async function getCommitHashes(githubUrl: string): Promise<Response[]> {
   // Fetching entire detail of the repository
   const { data } = await octokit.rest.repos.listCommits({
     owner: githubUrl.split("/")[3],
     repo: githubUrl.split("/")[4],
-  })
+  });
 
-  // Filtering data
-  const responses = data.map(commit => ({
+  // Sorting data in descending order
+  const sortedCommits = data.sort((a, b) => {
+    const dateA = b.commit.author?.date;
+    const dateB = a.commit.author?.date;
+    if (dateA && dateB) {
+      return new Date(dateA).getTime() - new Date(dateB).getTime();
+    } else {
+      // Handle the case where one or both dates are undefined
+      return 0;
+    }
+  });
+
+  // Slicing the first 10 commits
+  const slicedSortedCommits = sortedCommits.slice(0, 10).map((commit) => ({
     commitHash: commit.sha,
     commitMessage: commit.commit.message,
     authorName: commit.commit.author?.name,
     authorAvatarUrl: commit.author?.avatar_url,
-    committedAt: commit.commit.author?.date
-  }))
+    committedAt: commit.commit.author?.date,
+  }));
 
-  console.log("data", responses);
-  return responses;
+  console.log("Process Completed!");
+  console.log("Data", slicedSortedCommits);
+  return slicedSortedCommits;
 }
 
-console.log("All good! Octokit instance created.");
-getCommitHashes("https://github.com/fudge-fantastic/GenerativeAI")
-console.log("Process Completed!");
+getCommitHashes("https://github.com/fudge-fantastic/GenerativeAI");
